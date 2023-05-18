442ea2c9f35e44b693cfa4924f3d2227def2d110
ajuie <soukadrissi99@gmail.com>
2023-05-18 12:38:02 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ ** main - Entry pont
+ *
+ ** return : Always 0(Success)
+ **/
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
