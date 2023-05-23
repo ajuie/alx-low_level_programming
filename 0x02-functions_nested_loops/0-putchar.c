09eccc74c2eb6caf9f3777be4f1a510df8eb391f
ajuie <soukadrissi99@gmail.com>
2023-05-23 14:14:58 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
