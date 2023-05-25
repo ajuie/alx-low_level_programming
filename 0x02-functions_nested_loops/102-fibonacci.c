c9c64455ad2b6fba6a715f7a990418132ab067e5
ajuie <soukadrissi99@gmail.com>
2023-05-25 11:56:55 -0700

    task14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	unsigned long n1, n2, summ;
+
+	n1 = 0, n2 = 1;
+	for (i = 0; i <= 50; i++)
+	{
+		sum = n1 + n2;
+		printf("%lu", sum);
+
+		n1 = n2;
+		n2 = sum;
+		if (i != 50)
+		{
+			printf(", ");
+		}
+		else
+			printf("\n");
+	}
+}
