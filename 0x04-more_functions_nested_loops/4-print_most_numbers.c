cb601ca92e976db1dd7cf4cd089040ada8c928ef
ajuie <soukadrissi99@gmail.com>
2023-05-25 14:39:11 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - printd numbers except 2 and 4
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+}
