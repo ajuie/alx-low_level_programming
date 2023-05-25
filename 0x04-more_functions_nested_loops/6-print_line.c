849bb9ecd6229f80ff38f68b03ee30333f9fd2fd
ajuie <soukadrissi99@gmail.com>
2023-05-25 15:11:13 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: inputs an integer
+ *
+ * Return: 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
