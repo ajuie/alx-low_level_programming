80692d0f07d10c9a01899227d461c05938f54828
ajuie <soukadrissi99@gmail.com>
2023-06-12 14:04:56 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,9 @@
 
 /**
  * main - printd the name of the program
- * argc: the number of command-line arguments
- * argv: an array of command-line argument strings
+ * @argc: the number of command-line arguments
+ * @argv: an array of command-line argument strings
+ *
  * Return: Always o
  */
 int main(int argc, char *argv[])
