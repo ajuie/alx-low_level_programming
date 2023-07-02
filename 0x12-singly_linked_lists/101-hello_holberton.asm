930ef40c8383e032d22d03eca023ea0d63f57469
ajuie <soukadrissi@gmail.com>
2023-07-02 16:46:56 -0700

    task6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .rodata
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
+
+section .text
+global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
